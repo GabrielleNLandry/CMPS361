cd5c82ea70575eac79912dedde7206becda8d9cb
Gabrielle Landry <gnlandr@pointpark.edu>
2024-11-02 12:49:18 -0400

    added code to api.php

---
+++ Module2/api.php
@@ -0,0 +1,23 @@
+<?php
+    $url = "API URL GOES HERE";
+
+    $urlWITHPARAMS = $url;
+    // initialize the session
+    $session = curl_init();
+    //set the session options
+    curl_setopt($session, CURLOPT_URL, $urlWITHPARAMS); //endpoint
+    curl_setopt($session, CURLOPT_RETURNTRANSFER, $true); //return a response
+    // execute the request
+    $response = curl_exec($session);
+    //catch errors
+    if ($response === false) {
+        echo 'Error' . curl_error($session);
+    } else{
+        //return JSON
+        $responseData = json_decode($response, true);
+        header('Content-Type: application/json');
+        echo json_encode($responseData);
+    }
+    // close session
+    curl_close($session);
+?>
