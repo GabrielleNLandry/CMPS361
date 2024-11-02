488c4c37af795edb45f9f1fe96468c47531cc85b
Gabrielle Landry <gnlandr@pointpark.edu>
2024-11-02 13:08:20 -0400

    added conn.php file and code

---
+++ conn.php
@@ -0,0 +1,25 @@
+<?php
+// authentication credentials
+$host = "localhost";
+$port = "5432";
+$dbname = "inventory";
+$username = "postgres";
+$password = "Gabela2002!";
+
+//Connection String
+$dsn = "pgsql:host=$host;dbname=$dbname";
+
+try {
+    //Session
+    $instance = new PDO($dsn,$user,$password);
+
+    // Set an error alert
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO ::ERRMODE_EXCEPTION);
+
+    //Echo messages
+    echo "Successfully Connects to the Database";
+
+} catch (PDOException $e) {
+    echo "Connection Failed: " . $e->getMessage();
+}
+?>
