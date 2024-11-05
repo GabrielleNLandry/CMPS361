e519282c7871b0c917ae6e80312a700638bf43df
Gabrielle Landry <gnlandr@pointpark.edu>
2024-11-05 16:03:05 -0500

    changed some code

--- Module2/getData.php
+++ Module2/getData.php
@@ -1,39 +1,46 @@
 <?php
 
-$apiURL = "http://localhost:8000/Module2/conn.php";
+$apiURL = "http://localhost:8080/Module2/conn.php"; 
 
-//Fetch the data
+// Fetch the data
 $response = file_get_contents($apiURL);
-//Decode the JSON
-$data =json_decode($response, true);
 
-//Validate if the data exist
-if ($data && is_array($data)){
-    //Build out the table
-    echo"<table border = '1' cellpadding = '10'>";
-    echo "<thread>";
+// Validate if the response was successful
+if ($response === FALSE) {
+    die("Error occurred while fetching data from API: " . error_get_last()['message']);
+}
+
+// Decode the JSON
+$data = json_decode($response, true);
+
+// Validate if the data exists
+if ($data && is_array($data)) {
+    // Build out the table
+    echo "<table border='1' cellpadding='10'>";
+    echo "<thead>";
     echo "<tr>";
     echo "<th>bookname</th>";
     echo "<th>authorname</th>";
     echo "<th>isbn</th>";
     echo "<th>booktype</th>";
     echo "</tr>";
-    echo "</thread>";
+    echo "</thead>";
     echo "<tbody>";
 
-    //Loop through the data
-    foreach($data as $_POST) {
-        echo"<tr>";
-        echo"<td>" . htmlspecialchars($post['bookname']) . "</td>";
-        echo"<td>" . htmlspecialchars($post['authorname']) . "</td>";
-        echo"<td>" . htmlspecialchars($post['isbn']) . "</td>";
-        echo"<td>" . htmlspecialchars($post['booktype']) . "</td>";
+    // Loop through the data
+    foreach ($data as $post) { // Change $_POST to $post
+        echo "<tr>";
+        echo "<td>" . htmlspecialchars($post['bookname']) . "</td>";
+        echo "<td>" . htmlspecialchars($post['authorname']) . "</td>";
+        echo "<td>" . htmlspecialchars($post['isbn']) . "</td>";
+        echo "<td>" . htmlspecialchars($post['booktype']) . "</td>";
+        echo "</tr>"; // Added closing row tag here
     }
-    echo "<tbody>";
+    echo "</tbody>";
     echo "</table>";
 
 } else {
-    echo "Sorry no data is available.";
+    echo "Sorry, no data is available.";
 }
 
 ?>
